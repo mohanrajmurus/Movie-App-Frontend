e5e7196ec7d7a97a2b2a7651fa3d93241ab2e348
mohanrajmurus <mohanrajmurus@gmail.com>
2023-07-12 12:57:13 +0530

    store user data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Router from './routes/Router'
+import { useQueryClient } from 'react-query'
+
 const App = () => {
+  const queryClient = useQueryClient()
+  const data = JSON.parse(localStorage.getItem('user'))
+  queryClient.setQueryData('user',data)
   return (
     <div className='w-full'>
      <Router/>
--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -1,10 +1,13 @@
-import React from "react"
+import React, { useState } from "react"
 import avatar from "../../assets/Images/avatar.png"
 import { useNavigate } from "react-router-dom"
+import {useQueryClient} from 'react-query'
 import Btn from '../Btn';
 const Header = () => {
-  const user = true
+  const queryClient = useQueryClient()
+  const user = queryClient.getQueryData('user')
   const navigate = useNavigate()
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <div className="w-full  bg-black sticky top-0 z-10 mb-5">
       <nav className="w-11/12 h-[10vh] mx-auto flex justify-between items-center">
@@ -14,8 +17,16 @@ const Header = () => {
         >
           Movie Time
         </span>
-        <div>
-          {user ? <span onClick={() => navigate('user/login')}><Btn value={'Login'}/></span>:<img src={avatar} alt="prfile-pic" className="w-10 h-10" />}
+        <div className="relative">
+          {!user ? <span onClick={() => navigate('user/login')}><Btn value={'Login'}/></span>:<img src={avatar} alt="prfile-pic" className="w-10 h-10" onClick={()=>setIsOpen(!isOpen)}/>}
+          {isOpen ? <div className="w-52 text-white absolute top-12 rounded-2xl right-1 flex flex-col items-center space-y-3 bg-gray-800">
+            <span className="w-full text-center text-lg font-semibold px-3 py-2 hover:bg-red-500 cursor-pointer rounded-2xl">Profile</span>
+            <span className="w-full text-center text-lg font-semibold px-3 py-2 hover:bg-red-500 cursor-pointer rounded-2xl" onClick={() => {
+              localStorage.removeItem('user')
+              queryClient.removeQueries({queryKey:['user']})
+              setIsOpen(false)
+            }}>Logout</span>
+          </div>:<></>}
         </div>
       </nav>
     </div>
