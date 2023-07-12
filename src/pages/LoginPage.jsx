1cd367bdc0311b7b2aa46cd47963ef97bb0d09ab
mohanrajmurus <mohanrajmurus@gmail.com>
2023-07-12 10:28:29 +0530

    lognpage responsive

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,13 +8,13 @@ const LoginPage = () => {
   const navigate = useNavigate()
   return (
     <div className="w-11/12">
-      <div className="w-full lg:w-1/2 mx-auto">
+      <div className="w-full ml-4 lg:w-1/2 lg:mx-auto">
         <div className="w-full flex flex-col items-center space-y-8">
           <span className="text-white text-2xl font-bold">Login</span>
           <LoginWithBtn AuthProvider={FcGoogle} text={"Login With Google"} />
           <LoginWithBtn AuthProvider={FaApple} text={"Login With Apple"} />
           <span className="text-white">or</span>
-          <form className="w-1/2">
+          <form className="w-full md:w-1/2">
             <input
               type="email"
               className="w-full px-3 py-2 rounded-xl outline-none border-2 focus:border-red-300 mb-4"
