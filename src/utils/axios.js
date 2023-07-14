ce3ba9a2056710acb5cbafef6dc80af6eb825227
mohanrajmurus <mohanrajmurus@gmail.com>
2023-07-14 13:10:30 +0530

    auth token

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 const url = API__URL
-const token = JSON.parse(sessionStorage.getItem('user'))?.token
-console.log(token);
-const instance = axios.create({
- baseURL:url,
- headers:{
-    Authorization: `Bearer ${token}`
- }
-})
+export const instance = axios.create({
+   baseURL:url
+  })
+function setAuthToken(token){
+   console.log(token);
+   instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
 
-export default instance
+export default setAuthToken;
