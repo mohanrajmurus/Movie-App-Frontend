037b8faf4f909c30e659632e5a9d1a1addd4bc6d
mohanrajmurus <mohanrajmurus@gmail.com>
2023-07-19 11:59:42 +0530

    bg-color

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -1,8 +1,9 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
 
-const Btn = ({value,width,type,bg}) => {
+const Btn = ({value,width,type}) => {
   return (
-    <button className={`text-white text-lg px-4 pt-1 pb-2 bg-${bg || 'red-500'} rounded-lg w-${width}`} type={type}>{value}</button>
+    <button className={`text-white text-lg px-4 pt-1 pb-2 bg-red-600 rounded-lg w-${width}`} type={type}>{value}</button>
   )
 }
 
